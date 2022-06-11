5e8d475fd5fc37086a58892af359ce17099bb66c
Morgan <cromorg@outlook.com>
2022-06-11 09:28:46 +0200

    init spring web

--- backend/decisio/src/main/java/com/backend/decisio/DecisioApplication.java
+++ backend/decisio/src/main/java/com/backend/decisio/DecisioApplication.java
@@ -1,4 +1,4 @@
-package com.api.decisio;
+package com.backend.decisio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- backend/decisio/src/test/java/com/backend/decisio/DecisioApplicationTests.java
+++ backend/decisio/src/test/java/com/backend/decisio/DecisioApplicationTests.java
@@ -1,4 +1,4 @@
-package com.api.decisio;
+package com.backend.decisio;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
