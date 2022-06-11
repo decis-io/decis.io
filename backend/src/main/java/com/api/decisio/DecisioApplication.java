23c3dd44cc0fc9b6a4da10a78a6ef205c086714f
Morgan Crociati <cromorg@LAPTOP-MORGAN.localdomain>
2022-06-11 09:20:35 +0200

    init spring backend

---
+++ backend/src/main/java/com/api/decisio/DecisioApplication.java
@@ -0,0 +1,13 @@
+package com.api.decisio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DecisioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DecisioApplication.class, args);
+	}
+
+}
